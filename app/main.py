7eca0ca936ef86833bb77d775a2d0d8b500ccefb
k.nishie <k.nishie@mtadminnoMacBook-Air-6.local>
2025-03-23 23:56:41 +0900

    Update RFID Cloud Run build

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 import uvicorn
 from fastapi import FastAPI
-from app.endpoints import rfid_large, rfid_small, sync_large_rfid,sync_picking,update_inventory_large_receiving
+from app.endpoints import rfid_large, rfid_small, sync_large_rfid,sync_picking,update_inventory_large_receiving,enqueue_large_rfid
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -11,6 +11,7 @@ app.include_router(rfid_small.router)
 app.include_router(sync_large_rfid.router)
 app.include_router(sync_picking.router)
 app.include_router(update_inventory_large_receiving.router)
+app.include_router(enqueue_large_rfid.router)
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8080)
