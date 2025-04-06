abd319d87125b89d2371ece3081c71031141ded5
k.nishie <k.nishie@mtadminnoMacBook-Air-6.local>
2025-04-06 20:24:09 +0900

    prefix削除

--- app/endpoints/picking/__init__.py
+++ app/endpoints/picking/__init__.py
@@ -3,5 +3,5 @@ from app.endpoints.picking.sync_picking import router as sync_router
 from app.endpoints.picking.update_inventory_picking import router as update_router
 
 router = APIRouter()
-router.include_router(sync_router, prefix="/picking")
-router.include_router(update_router, prefix="/picking")
+router.include_router(sync_router)
+router.include_router(update_router)
--- app/endpoints/stockhouse/__init__.py
+++ app/endpoints/stockhouse/__init__.py
@@ -2,4 +2,4 @@ from fastapi import APIRouter
 from app.endpoints.stockhouse.sync_linen_stockhouse import router as sync_router
 
 router = APIRouter()
-router.include_router(sync_router, prefix="/stockhouse")
+router.include_router(sync_router)
